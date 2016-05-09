27c63f732733bf91bfeafb8c54b79af02d78b36d
Immanuel Trummer <immanuel.trummer@epfl.ch>
2016-05-09 16:35:59 +0200

    started DP optimizer

---
+++ components/src/main/scala/ch/epfl/data/dblab/frontend/optimizer/QueryPlanDPoptimizer.scala
@@ -0,0 +1,23 @@
+package ch.epfl.data
+package dblab
+package frontend
+package optimizer
+
+import schema._
+import parser._
+import parser.OperatorAST._
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * Query optimizer that implements a variant of the classical
+ * dynamic programming algorithm by Selinger.
+ *
+ * @author Immanuel Trummer
+ */
+class QueryPlanDPoptimizer extends QueryPlanOptimizer {
+  def optimize(qp: QueryPlanTree): QueryPlanTree = {
+  	// Split query tree into SPJ sub-queries whose join order is optimized independently
+  	// 
+    qp
+  }
+}
